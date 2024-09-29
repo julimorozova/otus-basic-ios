f3c8944d75c286fa96360673730bca3a4d908871
Julia Morozova <juliamorozova@fox-2.local>
2024-09-29 13:17:22 +0200

    add hw 4

--- Otus.playground/Pages/4-lesson-hw.xcplaygroundpage/Contents.swift
+++ Otus.playground/Pages/4-lesson-hw.xcplaygroundpage/Contents.swift
@@ -1,7 +1,27 @@
-//: [Previous](@previous)
+/*
+ Сгенерировать массив случайных чисел (например 200 чисел). Найти индекс первого повторяющегося числа в массиве. Если все числа разные - то -1.
+ Пример : [3, 4, 5, 6, 8, 78, 67, 4, 88] - 4, индекс 1
+ */
 
-import Foundation
+var nums: [Int] = []
 
-var greeting = "Hello, playground"
+for num in 0...199 {
+    nums.append(Int.random(in: 0...1000))
+}
 
-//: [Next](@next)
+var index = -1
+
+for i in 0..<nums.count {
+    if index != -1 {
+        break
+    }
+    
+    for j in i + 1..<nums.count {
+        if nums[i] == nums[j] {
+            index = i
+            break
+        }
+    }
+}
+
+print(index)
